0bddd96c46b858eb7d868ffd5b4f513aee7dfb90
rober0xf <robertinopili13@gmail.com>
2025-02-25 05:35:05 -0300

    routes: contact

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<h1 class="text-6xl">Contact me</h1>
+
+<p>the best way to get in touch is through discord</p>
+
+<Button
+	onclick={() => {
+		console.log('you clicked this');
+	}}
+	href="/discord">follow on discord</Button
+>
+>
+<p>you can follow us on:</p>
+
+<ul class="flex flex-col gap-4 py-4">
+	<li><Button variant="outline" href="youtube">follow on youtube</Button></li>
+	<li><Button variant="outline" href="/twitter">follow on twitter</Button></li>
+</ul>
