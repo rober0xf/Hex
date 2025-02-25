897ec8a37c2fd6fe0e63bf4197672f3f5e5d7e36
rober0xf <robertinopili13@gmail.com>
2025-02-25 05:33:45 -0300

    components: button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { Button, type ButtonRootProps } from 'bits-ui';
+
+	// we can make the Button dynamic like this:
+	type Props = {
+		children: Snippet; // snippet let us to use other elements, not just a string
+	} & ButtonRootProps;
+
+	let { children, ...restProps }: Props = $props(); // restProps is for onClick action for example
+</script>
+
+<Button.Root class="rounded-xl bg-orange-400 px-4 py-2 text-orange-200" {...restProps}
+	>{@render children()}
+</Button.Root>
