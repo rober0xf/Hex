0ca5019bb6667ae52693e9b8b44f3645f3d1e9de
rober0xf <robertinopili13@gmail.com>
2025-02-25 05:35:37 -0300

    routes: program

---
+++ src/routes/program/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<h1 class="text-6xl">Week's program</h1>
+<ul>
+	<li>
+		<h2>chest</h2>
+		<p>military</p>
+	</li>
+	<li>
+		<h2>lower</h2>
+		<p>squat</p>
+	</li>
+	<li>
+		<h2>arm</h2>
+		<p>press</p>
+	</li>
+	<li>
+		<h2>cardio</h2>
+		<p>run</p>
+	</li>
+</ul>
+<Button
+	onclick={() => {
+		console.log('someone downloaded it');
+	}}>download</Button
+>
+
+<p>want an own program?</p>
+<Button href="/create">create your own program</Button>
