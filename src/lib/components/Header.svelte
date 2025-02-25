27a018e7c46a8ff63dad76bb95dee21ad8e33b9e
rober0xf <robertinopili13@gmail.com>
2025-02-25 05:34:09 -0300

    components: header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Button } from './ui/button';
+	import ThemeToggleIcon from './ThemeToggleIcon.svelte';
+</script>
+
+<header class="flex items-center justify-between px-inline">
+	<Button class="font-bold" variant="ghost" href="/">Hex Fitness</Button>
+	<div>
+		<nav class="flex items-center">
+			<Button variant="ghost" href="/contact">Contact</Button>
+		</nav>
+		<Button
+			variant="ghost"
+			aria-label="Toggle Theme"
+			onclick={() => {
+				window.toggleTheme?.();
+			}}><ThemeToggleIcon /></Button
+		>
+	</div>
+</header>
